77fa35c2ffb245425e9cf770841fd3987e85455f
Khalil Ouali <khalilouali01@gmail.com>
2024-02-22 11:26:34 +0100

    Initial commit

---
+++ Drivers/BSP/STM32C0xx_Nucleo/stm32c0xx_nucleo.h
@@ -0,0 +1,295 @@
+/**
+  ******************************************************************************
+  * @file    stm32c0xx_nucleo.h
+  * @author  MCD Application Team
+  * @brief   This file contains definitions for:
+  *          - LEDs and push-button available on STM32C0xx-Nucleo Kit
+  *            from STMicroelectronics
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef STM32C0XX_NUCLEO_H
+#define STM32C0XX_NUCLEO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32c0xx_nucleo_conf.h"
+#include "stm32c0xx_nucleo_errno.h"
+
+#if (USE_BSP_COM_FEATURE > 0)
+#if (USE_COM_LOG > 0)
+#ifndef __GNUC__
+#include <stdio.h>
+#endif /* __GNUC__ */
+#endif /* USE_COM_LOG */
+#endif /* USE_BSP_COM_FEATURE */
+
+/** @addtogroup BSP
+  * @{
+  */
+
+/** @defgroup STM32C0XX_NUCLEO STM32C0XX_NUCLEO
+  * @{
+  */
+
+/** @defgroup STM32C0XX_NUCLEO_LOW_LEVEL LOW LEVEL
+  * @{
+  */
+
+/**
+  * @brief Define for STM32C0XX_NUCLEO board
+  */
+#if !defined (USE_STM32C0XX_NUCLEO)
+#define USE_STM32C0XX_NUCLEO
+#endif /* !defined (USE_STM32C0XX_NUCLEO) */
+
+/** @defgroup STM32C0XX_NUCLEO_LOW_LEVEL_Exported_Types LOW LEVEL Exported Types
+  * @{
+  */
+
+typedef enum
+{
+  LED4 = 0,
+  LED_GREEN = LED4,
+  LEDn
+} Led_TypeDef;
+
+typedef enum
+{
+  BUTTON_USER = 0U,
+  BUTTONn
+} Button_TypeDef;
+
+typedef enum
+{
+  BUTTON_MODE_GPIO = 0,
+  BUTTON_MODE_EXTI = 1
+} ButtonMode_TypeDef;
+
+#if (USE_BSP_COM_FEATURE > 0)
+typedef enum
+{
+  COM1 = 0U,
+  COMn
+} COM_TypeDef;
+
+typedef enum
+{
+  COM_STOPBITS_1     =   UART_STOPBITS_1,
+  COM_STOPBITS_2     =   UART_STOPBITS_2,
+} COM_StopBitsTypeDef;
+
+typedef enum
+{
+  COM_PARITY_NONE     =  UART_PARITY_NONE,
+  COM_PARITY_EVEN     =  UART_PARITY_EVEN,
+  COM_PARITY_ODD      =  UART_PARITY_ODD,
+} COM_ParityTypeDef;
+
+typedef enum
+{
+  COM_HWCONTROL_NONE    =  UART_HWCONTROL_NONE,
+  COM_HWCONTROL_RTS     =  UART_HWCONTROL_RTS,
+  COM_HWCONTROL_CTS     =  UART_HWCONTROL_CTS,
+  COM_HWCONTROL_RTS_CTS =  UART_HWCONTROL_RTS_CTS,
+} COM_HwFlowCtlTypeDef;
+
+typedef enum
+{
+  COM_WORDLENGTH_7B = UART_WORDLENGTH_7B,
+  COM_WORDLENGTH_8B = UART_WORDLENGTH_8B,
+  COM_WORDLENGTH_9B = UART_WORDLENGTH_9B,
+} COM_WordLengthTypeDef;
+
+typedef struct
+{
+  uint32_t               BaudRate;
+  COM_WordLengthTypeDef  WordLength;
+  COM_StopBitsTypeDef    StopBits;
+  COM_ParityTypeDef      Parity;
+  COM_HwFlowCtlTypeDef   HwFlowCtl;
+} COM_InitTypeDef;
+
+#if (USE_HAL_UART_REGISTER_CALLBACKS == 1)
+typedef struct
+{
+  void (* pMspInitCb)(UART_HandleTypeDef *);
+  void (* pMspDeInitCb)(UART_HandleTypeDef *);
+} BSP_COM_Cb_t;
+#endif /* (USE_HAL_UART_REGISTER_CALLBACKS == 1) */
+
+#define MX_UART_InitTypeDef COM_InitTypeDef
+
+#endif /* USE_BSP_COM_FEATURE */
+
+/**
+  * @}
+  */
+
+/** @defgroup STM32C0XX_NUCLEO_LOW_LEVEL_Exported_Constants LOW LEVEL Exported Constants
+  * @{
+  */
+
+/**
+  * @brief STM32C0XX NUCLEO BSP Driver version number V1.0.0
+  */
+#define STM32C0XX_NUCLEO_BSP_VERSION_MAIN   (0x01U) /*!< [31:24] main version */
+#define STM32C0XX_NUCLEO_BSP_VERSION_SUB1   (0x00U) /*!< [23:16] sub1 version */
+#define STM32C0XX_NUCLEO_BSP_VERSION_SUB2   (0x00U) /*!< [15:8]  sub2 version */
+#define STM32C0XX_NUCLEO_BSP_VERSION_RC     (0x00U) /*!< [7:0]  release candidate */
+#define STM32C0XX_NUCLEO_BSP_VERSION        ((STM32C0XX_NUCLEO_BSP_VERSION_MAIN << 24)\
+                                             |(STM32C0XX_NUCLEO_BSP_VERSION_SUB1 << 16)\
+                                             |(STM32C0XX_NUCLEO_BSP_VERSION_SUB2 << 8 )\
+                                             |(STM32C0XX_NUCLEO_BSP_VERSION_RC))
+
+#define STM32C0XX_NUCLEO_BSP_BOARD_NAME  "NUCLEO-C031C6";
+#define STM32C0XX_NUCLEO_BSP_BOARD_ID    "MB1717B";
+
+/** @defgroup STM32C0XX_NUCLEO_LOW_LEVEL_LED LOW LEVEL LED
+  * @{
+  */
+#define LED4_PIN                                GPIO_PIN_5
+#define LED4_GPIO_PORT                          GPIOA
+#define LED4_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOA_CLK_ENABLE()
+#define LED4_GPIO_CLK_DISABLE()                 __HAL_RCC_GPIOA_CLK_DISABLE()
+
+
+
+/**
+  * @}
+  */
+
+/** @defgroup STM32C0XX_NUCLEO_LOW_LEVEL_BUTTON LOW LEVEL BUTTON
+  * @{
+  */
+/* Button state */
+#define BUTTON_RELEASED                    0U
+#define BUTTON_PRESSED                     1U
+
+/**
+  * @brief Key push-button
+  */
+#define BUTTON_USER_PIN                       GPIO_PIN_13
+#define BUTTON_USER_GPIO_PORT                 GPIOC
+#define BUTTON_USER_GPIO_CLK_ENABLE()         __HAL_RCC_GPIOC_CLK_ENABLE()
+#define BUTTON_USER_GPIO_CLK_DISABLE()        __HAL_RCC_GPIOC_CLK_DISABLE()
+#define BUTTON_USER_EXTI_IRQn                 EXTI4_15_IRQn
+#define BUTTON_USER_EXTI_LINE                 GPIO_PIN_13
+
+/**
+  * @}
+  */
+
+/** @defgroup STM32C0XX_NUCLEO_LOW_LEVEL_COM LOW LEVEL COM
+  * @{
+  */
+/**
+  * @brief Definition for COM port1, connected to UART1
+  */
+#if (USE_BSP_COM_FEATURE > 0)
+#define COM1_UART                     USART2	// fix_MX_codegen.py
+#define COM1_CLK_ENABLE()             __HAL_RCC_USART2_CLK_ENABLE()	// fix_MX_codegen.py
+#define COM1_CLK_DISABLE()            __HAL_RCC_USART2_CLK_DISABLE()	// fix_MX_codegen.py
+
+#define COM1_TX_PIN                   GPIO_PIN_2	// fix_MX_codegen.py
+#define COM1_TX_GPIO_PORT             GPIOA	// fix_MX_codegen.py
+#define COM1_TX_GPIO_CLK_ENABLE()     __HAL_RCC_GPIOA_CLK_ENABLE()	// fix_MX_codegen.py
+#define COM1_TX_GPIO_CLK_DISABLE()    __HAL_RCC_GPIOA_CLK_DISABLE()	// fix_MX_codegen.py
+#define COM1_TX_AF                    GPIO_AF1_USART2	// fix_MX_codegen.py
+
+#define COM1_RX_PIN                   GPIO_PIN_3	// fix_MX_codegen.py
+#define COM1_RX_GPIO_PORT             GPIOA	// fix_MX_codegen.py
+#define COM1_RX_GPIO_CLK_ENABLE()     __HAL_RCC_GPIOA_CLK_ENABLE()	// fix_MX_codegen.py
+#define COM1_RX_GPIO_CLK_DISABLE()    __HAL_RCC_GPIOA_CLK_DISABLE()	// fix_MX_codegen.py
+#define COM1_RX_AF                    GPIO_AF1_USART2	// fix_MX_codegen.py
+#define COM_POLL_TIMEOUT             HAL_MAX_DELAY	// fix_MX_codegen.py
+#endif /* USE_BSP_COM_FEATURE */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/** @addtogroup STM32C0XX_NUCLEO_LOW_LEVEL_Exported_Variables
+  * @{
+  */
+extern EXTI_HandleTypeDef hpb_exti[];
+#if (USE_BSP_COM_FEATURE > 0)
+extern UART_HandleTypeDef hcom_uart[];
+extern USART_TypeDef *COM_USART[];
+#endif /* USE_BSP_COM_FEATURE */
+
+/**
+  * @}
+  */
+
+/** @defgroup STM32C0XX_NUCLEO_LOW_LEVEL_Exported_FunctionsPrototypes LOW LEVEL Exported Functions Prototypes
+  * @{
+  */
+int32_t  BSP_GetVersion(void);
+const uint8_t *BSP_GetBoardName(void);
+const uint8_t *BSP_GetBoardID(void);
+int32_t  BSP_LED_Init(Led_TypeDef Led);
+int32_t  BSP_LED_DeInit(Led_TypeDef Led);
+int32_t  BSP_LED_On(Led_TypeDef Led);
+int32_t  BSP_LED_Off(Led_TypeDef Led);
+int32_t  BSP_LED_Toggle(Led_TypeDef Led);
+int32_t  BSP_LED_GetState(Led_TypeDef Led);
+int32_t  BSP_PB_Init(Button_TypeDef Button, ButtonMode_TypeDef ButtonMode);
+int32_t  BSP_PB_DeInit(Button_TypeDef Button);
+int32_t  BSP_PB_GetState(Button_TypeDef Button);
+void     BSP_PB_IRQHandler(Button_TypeDef Button);
+void     BSP_PB_Callback(Button_TypeDef Button);
+
+#if (USE_BSP_COM_FEATURE > 0)
+int32_t  BSP_COM_Init(COM_TypeDef COM, COM_InitTypeDef *COM_Init);
+int32_t  BSP_COM_DeInit(COM_TypeDef COM);
+#if (USE_COM_LOG > 0)
+int32_t  BSP_COM_SelectLogPort(COM_TypeDef COM);
+#endif /* USE_COM_LOG */
+
+#if (USE_HAL_UART_REGISTER_CALLBACKS == 1)
+int32_t BSP_COM_RegisterDefaultMspCallbacks(COM_TypeDef COM);
+int32_t BSP_COM_RegisterMspCallbacks(COM_TypeDef COM, BSP_COM_Cb_t *Callback);
+#endif /* USE_HAL_UART_REGISTER_CALLBACKS */
+HAL_StatusTypeDef MX_USART2_Init(UART_HandleTypeDef *huart, MX_UART_InitTypeDef *COM_Init);	// fix_MX_codegen.py
+#endif /* USE_BSP_COM_FEATURE */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* STM32C0XX_NUCLEO_H */
---
+++ README.md
@@ -0,0 +1,115 @@
+# FreeRTOS / HAL on STM32 Nucleo64 C031C6 with Wokwi Simulation
+
+## Description
+
+- Framework: `FreeRTOS / HAL`
+- Toolchain: `ARM GCC`
+- Builder: `Make`
+- Configuration: `STM32CubeMX`
+- Board: `Nucleo C031C6`
+  - MCU: `STM32C031C6`
+    - CPU: `ARM Cortex-M0+`
+    - Clock: `4-48 MHz`
+    - RAM: `12 KB`
+    - Flash: `32 KB`
+- IDE: `VSCode`
+- Simulation: `Wokwi`
+- CI/CD: `GitHub Actions` + `Wokwi CI`
+
+## Prerequisites
+
+- [Arm GNU Toolchain](https://developer.arm.com/downloads/-/arm-gnu-toolchain-downloads) for compilation
+- [GNU Make](https://www.gnu.org/software/make/) ([Make for Windows](https://gnuwin32.sourceforge.net/packages/make.htm)) for building
+- [STM32CubeMX](https://www.st.com/en/development-tools/stm32cubemx.html) for configuration / code generation
+
+## Recommended usage
+
+1. Clone this repo
+2. Open in [VSCode](https://code.visualstudio.com/)
+3. Install the `[recommended extensions](.vscode/extensions.json)
+4. Run VSCode debugger on `Build & Debug` task
+
+VSCode will automatically build the project, start the Wokwi simulator and attach the debugger.
+The simulation will be paused for the debugger to attach. Once ready, you can continue by clicking ⏯️ or pressing `F5`.
+
+## Alternate usage
+
+### Build
+
+Run `make` from terminal or use the [Makefile Tools](https://marketplace.visualstudio.com/items?itemName=ms-vscode.makefile-tools) extension.
+
+By default the build will be in debug mode. To build for release, set `DEBUG=0` in the [Makefile](Makefile).
+
+### Simulate
+
+1. Install [Wokwi extension](https://marketplace.visualstudio.com/items?itemName=wokwi.wokwi-vscode) for VSCode
+2. Command Palette -> `Wokwi: Request a New License` and get license from wokwi.com
+3. Command Palette -> `Wokwi: Select Config File` and select `Wokwi/wokwi.toml`
+4. Command Palette -> `Wokwi: Start Simulator`
+
+Relevant files:
+
+- [`Wokwi/wokwi.toml`](Wokwi/wokwi.toml) - See [documentation](https://docs.wokwi.com/vscode/project-config).
+- [`Wokwi/diagram.json`](Wokwi/diagram.json) - See [diagram format](https://docs.wokwi.com/diagram-format) and [board details](https://docs.wokwi.com/parts/board-st-nucleo-c031c6).
+
+Notes:
+
+- You will see the serial output directly in the Wokwi terminal.
+- The simulation only runs while the Wokwi tab is visible in VSCode.
+- The simulation requires an internet connection. You can get a license for offline use.
+- You can add more components to the simulation by editing [`Wokwi/diagram.json`](Wokwi/diagram.json) and restarting the simulator.
+
+### Debug
+
+1. Install [C/C++ extension](https://marketplace.visualstudio.com/items?itemName=ms-vscode.cpptools) for VSCode
+2. Start simulation
+3. Run VSCode debugger on `Debug` task
+
+## Further usage
+
+### Serial communication
+
+1. Install [Serial Monitor extension](https://marketplace.visualstudio.com/items?itemName=ms-vscode.vscode-serial-monitor) for VSCode
+2. Command Palette -> `Serial Monitor: Focus on Serial Monitor View`
+3. Monitor Mode: `TCP` - Host: `localhost` - Port: `4000`
+4. Start simulation
+5. Click `Start Monitoring`
+
+Notes:
+
+- The serial interface corresponds to the `VCOM` (BSP abstraction of `UART2`) on the Nucleo C031C6.
+- `rfc2217ServerPort = <port>` in [`Wokwi/wokwi.toml`](Wokwi/wokwi.toml) is used to forward the serial port to a local TCP port.
+- The serial monitor can be used to send data to the simulation.
+  - However, it does not append `\r` or `\n` to the input, and there is no way to send either. So use another separator for inputs.
+
+### FreeRTOS inspection
+
+To monitor the FreeRTOS tasks and queues
+
+1. Install [Embedded Tools extension](https://marketplace.visualstudio.com/items?itemName=ms-vscode.vscode-embedded-tools) for VSCode
+2. Start debugging
+3. Go to the `Embedded Tools` tab in the panel (which contains the terminal etc.)
+
+### CI
+
+Configured in [`.github/workflows/ci.yml`](.github/workflows/ci.yml).
+Upon push to GitHub, the workflow will build and test the project in Wokwi simulator.
+See [Wokwi CI](https://docs.wokwi.com/wokwi-ci/getting-started) for more details.
+
+### STM32CubeMX
+
+This project was initially generated using STM32CubeMX.
+You can open [`Nucleo-C031C6-FreeRTOS.ioc`](Nucleo-C031C6-FreeRTOS.ioc) in STM32CubeMX to reconfigure the project.and regenerate the code.
+The generated code has some issues. Check or run [`fix_MX_codegen.py`](fix_MX_codegen.py) for the fixes.
+
+## Development
+
+The main user code is in [`Core/Src/main.c`](Core/Src/main.c) and [`Core/Src/app_freertos.c`](Core/Src/app_freertos.c).
+The FreeRTOS configuration is in [`Core/Inc/FreeRTOSConfig.h`](Core/Inc/FreeRTOSConfig.h).
+It is recommended that you write your code within the `USER CODE` sections, as STM32CubeMX will overwrite the rest of the files if you use it to reconfigure the project and regenerate the code. (Hence the need for the [`fix_MX_codegen.py`](fix_MX_codegen.py) script.)
+
+## Acknowledgements
+
+- Originally based on [Wokwi/stm32-hello-wokwi](https://github.com/wokwi/stm32-hello-wokwi).
+- Big thanks to the Wokwi team for their amazing work on the simulator and various integrations.
+- Also thanks to the Open-Source and Embedded communities online.
